67122881e981839fb1384fd08fbb9c131fc0730b
Joabe Santos <joabesilva1233@gmail.com>
2023-12-07 17:41:05 -0300

    feat: link header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -9,7 +9,7 @@ export const Header = () => {
             <div>
                 <img src={logo} alt='Logo' />
 
-                <a href="">Acessar Repositótio <img src={github_icon} alt="icon github" /></a>
+                <a href="https://github.com/joabesnts09/Effeito-Prarallax-joabesnts09" target='_blank"'><span>Acessar</span> Repositótio <img src={github_icon} alt="icon github" /></a>
             </div>
         </header>
     )
